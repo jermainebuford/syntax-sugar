b06c8bffc3ab2e9eb3a2bced9efa1579a072beeb
jermaine buford <bufordjermaine@outlook.com>
2022-06-03 12:05:31 -0400

    first init

---
+++ syntax sugar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace syntax_sugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 8;
+            string response;
+            if (a > 10)
+            {
+                Console.WriteLine($" {a} is greater than 10");
+
+            }
+            else
+            {
+                Console.WriteLine($" {a} is less than 10");
+            }
+
+
+            //var a = 10;
+           
+            //var response = (a > 9) ? $" {a} is greater than 9" : $"{a} is less than 9";
+            //Console.WriteLine(response);
+
+            
+
+        }
+    }
+}
